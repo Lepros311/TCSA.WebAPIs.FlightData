010de9209001d2fd4808dbae80bc65b454a8d344
Lepros311 <andy.strickland@gmail.com>
2025-06-02 10:45:18 -0400

    Initial commit

---
+++ Data/FlightsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TCSA.WebAPIs.FlightData.Models;
+
+namespace TCSA.WebAPIs.FlightData.Data;
+
+public class FlightsDbContext: DbContext
+{
+    public FlightsDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<Flight> Flights { get; set; }
+}
